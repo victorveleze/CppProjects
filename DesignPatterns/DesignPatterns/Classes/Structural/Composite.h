e88d6132c1039c1e6dcb04cec306d43506542a16
Victor <velezvictorv@gmail.com>
2021-12-23 16:48:15 -0300

    Adding Composite example

--- DesignPatterns/DesignPatterns/Classes/Structural/Composite.h
+++ DesignPatterns/DesignPatterns/Classes/Structural/Composite.h
@@ -44,3 +44,22 @@ private:
     std::vector<std::shared_ptr<Component>> components_;
 };
 
+/* Composite example:
+     const std::shared_ptr<Component> avatar { std::make_shared<Avatar>() };
+
+    const std::shared_ptr<Component> leftArm{std::make_shared<AvatarPart>("Left arm")};
+    avatar->addComponent(leftArm);
+    const std::shared_ptr<Component> rightArm{std::make_shared<AvatarPart>("Right arm")};
+    avatar->addComponent(rightArm);
+    const std::shared_ptr<Component> axe{std::make_shared<AvatarPart>("Axe")};
+    rightArm->addComponent(axe);
+    const std::shared_ptr<Component> candle{ std::make_shared<AvatarPart>("Candle") };
+    leftArm->addComponent(candle);
+
+    avatar->operation();
+
+    std::cout << "Let's imagine that the avatar stumbled and the axe felt \n";
+    avatar->removeComponent(leftArm);
+
+    avatar->operation();
+ */
